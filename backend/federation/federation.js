fb049d6cf1924e81e2e0e811371dcfa003e19d19
tshepobbd <tshepo.ncube@bbd.co.za>
2025-07-31 20:46:16 +0200

    sds

--- backend/federation/federation.js
+++ backend/federation/federation.js
@@ -2,12 +2,3 @@ import { createFederation, Person } from "@fedify/fedify";
 import { getLogger } from "@logtape/logtape";
 import { MemoryKvStore, InProcessMessageQueue } from "@fedify/fedify";
 import { postsService } from "./services/posts.js";
-
-const logger = getLogger("imageON");
-
-const federation = createFederation({
-  kv: new MemoryKvStore(),
-  queue: new InProcessMessageQueue(),
-});
-
-export default federation;
